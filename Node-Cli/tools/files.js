d9db96a22dc5936fd1e0e4c6061dfebcd85acd92
Cristalvaldez07 <vzcristal@gmail.coml>
2021-03-11 12:45:08 -0400

    Crea funcion loadTask()

--- Node-Cli/tools/files.js
+++ Node-Cli/tools/files.js
@@ -11,38 +11,19 @@ const filePath = path.join (__dirname, '../db/task.json');
 const loadTask = () => { 
     let listTask = {};
 
-    console.log ("Ruta absoluta del proceso node: ". yellow, process.cwd() ) 
+    console.log ("Ruta absoluta del proceso node: ".yellow, process.cwd() ) 
 
 
-    console.log ("Ruta absoluta de nuestra base de datos: ". yellow, filePath)
+    console.log ("Ruta absoluta de nuestra base de datos: ".yellow, filePath)
 
     try { 
         listTask = require(filePath);
     } catch (error) {
-        console.log ("Error cargando lista de bd". red, error);
+        console.log ("Error cargando lista de bd".red, error);
         listTask =  {}
     }
     return listTask ;
 }
 
 
-
-const saveTask = (listTask) => {
-
-    let data = JSON.stringify(listTask);
-
-
-    fs.writeFile(filePath, data, (error) => { 
-        if (error) {
-            throw new Error("Error Create by me: --", error);
-        }
-    
-    })
-
-}
-
-module.exports = { 
-    loadTask, 
-    saveTask 
-}
 
