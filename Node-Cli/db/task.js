4a2f059f4cc50293a5096f1203818b6fa8baab1b
Cristalvaldez07 <vzcristal@gmail.coml>
2021-03-17 07:59:56 -0400

    Configura argvs con opciones para los comandos

---
+++ Node-Cli/db/task.js
@@ -0,0 +1,62 @@
+
+
+require ('colors')
+const files = require ('./files');
+
+
+
+const createTask = (title,desc) =>  { 
+
+    let listTask = files.loadTask
+
+    if (title in listTask) { 
+        console.log("Esta tarea existe!".red) ;
+    } else { 
+
+        listTask [title] = { 
+            title,
+            desc,
+            completed: false 
+    }
+    files.saveTask(listTask);
+
+
+} 
+return listTask [title];
+}
+
+
+
+
+const updateTask = (title) => {
+
+    let list = files.loadTask ();
+
+    if (title in list) {
+        list [title] = {...list[title], completed: true}
+        files.saveTask(list);
+    } else {
+        console.log(`Este titulo no existe ${title}`.red) ;
+    }
+}
+
+
+
+
+const eraseTask = (title) => {
+let list = files.loadTask ();
+
+if (title in list) {
+    delete list [title]
+    files.saveTask(list);
+    } else {
+    console.log (`Este titulo no existe ${title}`.red);
+    }
+}
+
+
+module.exports = {
+ createTask,
+ eraseTask,
+ updateTask
+}
